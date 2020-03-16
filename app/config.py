ef0aa837fc8e91de7d50180e2fe8477e377b85ad
Philip Wafula <philipwafula2@gmail.com>
2020-03-16 10:32:55 +0300

    Refactored config.py to match configuration set up.

--- app/config.py
+++ app/config.py
@@ -3,8 +3,8 @@ import os
 
 API_VERSION = '0.0.1'
 
-# get absolute path for config file
-CONFIG_FILE_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
+# get absolute path for config file [/faulu-api-demo/config]
+CONFIG_FILE_DIRECTORY = os.path.abspath(os.path.join(os.getcwd(), ".."))
 
 # get environment on which API is running
 DEPLOYMENT_ENVIRONMENT = os.environ.get('DEPLOYMENT_NAME') or 'development'
@@ -15,22 +15,22 @@ print('CURRENT DEPLOYMENT ENVIRONMENT: ' + DEPLOYMENT_ENVIRONMENT)
 # get config file name
 CONFIG_FILENAME = '{}_config.ini'.format(DEPLOYMENT_ENVIRONMENT.lower())
 
-# define config parsers
-specific_config_file_parser = configparser.ConfigParser()
+# define config file parser
+config_file_parser = configparser.ConfigParser()
 
 # get folder for path for config files
-specific_config_file_folder_path = os.path.join(CONFIG_FILE_DIRECTORY, 'config/' + CONFIG_FILENAME)
+config_file_folder_path = os.path.join(CONFIG_FILE_DIRECTORY, 'config/' + CONFIG_FILENAME)
 
 
 # check if specific config files are present
-if not os.path.isfile(specific_config_file_folder_path):
+if not os.path.isfile(config_file_folder_path):
     raise Exception("Missing Config File: {}".format(CONFIG_FILENAME))
 
 # read files from config file paths
-specific_config_file_parser.read(specific_config_file_folder_path)
+config_file_parser.read(config_file_folder_path)
 
 # get deployment name
-DEPLOYMENT_NAME = specific_config_file_parser['APP']['DEPLOYMENT_NAME']
+DEPLOYMENT_NAME = config_file_parser['APP']['DEPLOYMENT_NAME']
 
 # check that the deployment name specified by the env matches the one in the config file
 if DEPLOYMENT_ENVIRONMENT.lower() != DEPLOYMENT_NAME.lower():
@@ -39,32 +39,32 @@ if DEPLOYMENT_ENVIRONMENT.lower() != DEPLOYMENT_NAME.lower():
         DEPLOYMENT_NAME.lower()))
 
 # define checks for deployment environment
-IS_TEST = specific_config_file_parser['APP'].getboolean('IS_TEST', False)
-IS_PRODUCTION = specific_config_file_parser['APP'].getboolean('IS_PRODUCTION')
+IS_TEST = config_file_parser['APP'].getboolean('IS_TEST', False)
+IS_PRODUCTION = config_file_parser['APP'].getboolean('IS_PRODUCTION')
 
 # get application host
-APP_HOST = specific_config_file_parser['APP']['HOST']
+APP_HOST = config_file_parser['APP']['HOST']
 
 # get application port
-APP_PORT = specific_config_file_parser['APP']['PORT']
+APP_PORT = config_file_parser['APP']['PORT']
 
 # define redis uri
-REDIS_URL = 'redis://' + specific_config_file_parser['REDIS']['URI']
+REDIS_URL = 'redis://' + config_file_parser['REDIS']['URI']
 
 # get database user
-DATABASE_USER = specific_config_file_parser['DATABASE'].get('user')
+DATABASE_USER = config_file_parser['DATABASE'].get('user')
 
 # get database password
-DATABASE_PASSWORD = specific_config_file_parser['DATABASE']['password']
+DATABASE_PASSWORD = config_file_parser['DATABASE']['password']
 
 # get database host
-DATABASE_HOST = specific_config_file_parser['DATABASE']['host']
+DATABASE_HOST = config_file_parser['DATABASE']['host']
 
 # get database name
-DATABASE_NAME = specific_config_file_parser['DATABASE'].get('database')
+DATABASE_NAME = config_file_parser['DATABASE'].get('database')
 
 # get database port
-DATABASE_PORT = specific_config_file_parser['DATABASE'].get('port')
+DATABASE_PORT = config_file_parser['DATABASE'].get('port')
 
 
 def get_database_uri(name, host, censored=True):
