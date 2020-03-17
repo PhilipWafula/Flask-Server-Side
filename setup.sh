0cec0271b4f8b79c39ab6c5d834c22da3ef82a52
SpencerOfwiti <maxspencer56@gmail.com>
2020-03-17 20:14:12 +0300

    Refactored setup script.

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 
 # install requirements
 cd app || exit
-pip install - requirements.txt
+pip3 install -r requirements.txt
 
 # return to parent directory
 cd ..
 
 # export path to python path
 ABSOLUTE_PATH_FOR_SCRIPT=$(realpath "$(dirname "${BASH_SOURCE[@]}")")
-export PYTHONPATH=$PYTHONPATH:$ABSOLUTE_PATH_FOR_SCRIPT/app
+export PYTHONPATH=$PYTHONPATH:$ABSOLUTE_PATH_FOR_SCRIPT
