6ef80558ed3f03028006ee210b565c8bf1fe6f9c
SpencerOfwiti <maxspencer56@gmail.com>
2020-03-17 20:39:24 +0300

    Initializes flask script cli access.

---
+++ app/manage.py
@@ -0,0 +1,15 @@
+from flask_migrate import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+from app.server import create_app
+from app.server import db
+
+app = create_app()
+migrate = Migrate(app=app, db=db)
+
+manager = Manager(app=app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
+
