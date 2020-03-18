9e2aa3686ae7f6257225a6fd2303908adcce239c
Philip Wafula <philipwafula2@gmail.com>
2020-03-18 16:22:53 +0300

    Adds exceptions.

---
+++ app/server/exceptions.py
@@ -0,0 +1,5 @@
+class IdentificationTypeNotFound(Exception):
+    """
+    Raise if an identification type is supplied that does not exist in the application's identification type constants.
+    """
+    pass
