54853bb58696c89bbe8e96a8ecf7dc9b752d7026
Philip Wafula <philipwafula2@gmail.com>
2020-03-19 21:22:27 +0300

    Implements registration API.

---
+++ app/server/api/auth.py
@@ -0,0 +1,36 @@
+from flask import Blueprint
+from flask import current_app
+from flask import jsonify
+from flask import make_response
+from flask import request
+from flask.views import MethodView
+
+from app.server import db
+from app.server.models.user import User
+from app.server.utils.user import process_create_or_update_user_request
+
+auth_blueprint = Blueprint('auth', __name__)
+
+
+class RegisterAPI(MethodView):
+    """
+    Create user
+    """
+
+    def post(self):
+        # get registration data
+        user_data = request.get_json()
+
+        response, status_code = process_create_or_update_user_request(user_attributes=user_data)
+        if status_code == 200:
+            db.session.commit()
+            status_code = 201
+
+        return make_response(jsonify(response), status_code)
+
+
+auth_blueprint.add_url_rule(
+    '/auth/register/',
+    view_func=RegisterAPI.as_view('register_api'),
+    methods=['POST']
+)
