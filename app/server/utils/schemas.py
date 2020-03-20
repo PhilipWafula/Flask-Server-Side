63077e0f2e9afe8bcffc6c185f608134c7ae37e5
Philip Wafula <philipwafula2@gmail.com>
2020-03-20 03:51:06 +0300

    Refactors schema to improve dump data.

--- app/server/utils/schemas.py
+++ app/server/utils/schemas.py
@@ -8,5 +8,5 @@ class BaseSchema(Schema):
         jit = toastedmarshmallow.Jit
 
     id = fields.Int(dump_only=True)
-    created = fields.DateTime(dump_only=True)
-    updated = fields.DateTime(dump_only=True)
+    created_at = fields.DateTime(dump_only=True)
+    updated_at = fields.DateTime(dump_only=True)
