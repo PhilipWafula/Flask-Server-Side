78c165c218aa2b6c9c5b2b16a0332ec5a0da914b
Philip Wafula <philipwafula2@gmail.com>
2020-03-20 17:34:34 +0300

    Adds role exceptions.

--- app/server/exceptions.py
+++ app/server/exceptions.py
@@ -3,3 +3,17 @@ class IdentificationTypeNotFound(Exception):
     Raise if an identification type is supplied that does not exist in the application's identification type constants.
     """
     pass
+
+
+class RoleNotFound(Exception):
+    """
+    Raise if provided role is not in organization's defined roles
+    """
+    pass
+
+
+class TierNotFound(Exception):
+    """
+    Raise if provided tier is not in organization's defined tier
+    """
+    pass
