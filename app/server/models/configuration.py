e90c8ce03110cff9c0d4851ad8cfef4fc622bc0d
philip <philipwafula2@gmail.com>
2020-03-28 00:39:37 +0300

    Adds access roles and tiers to support. - Creates two array columns to store an organization's admin roles and tiers if it is a tiered access control.

--- app/server/models/configuration.py
+++ app/server/models/configuration.py
@@ -1,6 +1,10 @@
+from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalchemy.ext.hybrid import hybrid_property
+
 from app.server import db
 from app.server.utils.enums.access_control_enums import AccessControlType
 from app.server.utils.models import BaseModel
+from app.server.utils.models import MutableList
 
 
 class Configuration(BaseModel):
@@ -9,5 +13,53 @@ class Configuration(BaseModel):
     """
     __tablename__ = 'configurations'
     access_control_type = db.Column(db.Enum(AccessControlType))
+    access_roles = db.Column(MutableList.as_mutable(ARRAY(db.String)))
+    access_tiers = db.Column(MutableList.as_mutable(ARRAY(db.String)))
+
     organization_id = db.Column(db.Integer, db.ForeignKey('organizations.id'))
     organization = db.relationship('Organization', back_populates='configurations')
+
+    @hybrid_property
+    def roles(self):
+        if self.access_roles is None:
+            return []
+        return self.access_roles
+
+    def set_access_role(self, roles_list):
+        for role in roles_list:
+            if not isinstance(role, str):
+                raise ValueError('Role {} should be a string.'.format(role))
+            role.upper()
+            self.access_roles.append(role)
+
+    def remove_specific_role(self, role):
+        if role not in self.access_roles:
+            raise ValueError('Role {} not found in access roles.'.format(role))
+        index = self.access_roles.index(role)
+        self.access_roles.pop(index)
+
+    def remove_all_roles(self):
+        self.access_roles = []
+
+    @hybrid_property
+    def tiers(self):
+        if self.access_tiers is None:
+            return []
+        return self.access_tiers
+
+    def set_access_tier(self, tiers_list):
+        for tier in tiers_list:
+            if not isinstance(tier, str):
+                raise ValueError('Tier {} should be a string.'.format(tier))
+            tier.upper()
+            self.access_tiers.append(tier)
+
+    def remove_specific_tier(self, tier):
+        if tier not in self.access_tiers:
+            raise ValueError('Tier {} not found in access tiers.'.format(tier))
+        index = self.access_tiers.index(tier)
+        self.access_tiers.pop(index)
+
+    def remove_all_tiers(self):
+        self.access_tiers = []
+
--- app/server/utils/models.py
+++ app/server/utils/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from sqlalchemy.ext.mutable import Mutable
 
 from app.server import db
 
@@ -9,3 +10,23 @@ class BaseModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+
+class MutableList(Mutable, list):
+    def append(self, value):
+        list.append(self, value)
+        self.changed()
+
+    def pop(self, index=0):
+        value = list.pop(self, index)
+        self.changed()
+        return value
+
+    @classmethod
+    def coerce(cls, key, value):
+        if not isinstance(value, MutableList):
+            if isinstance(value, list):
+                return MutableList(value)
+            return Mutable.coerce(key, value)
+        else:
+            return value
