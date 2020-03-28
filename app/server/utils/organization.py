913d704f612af753450c748b7ccf17df6c49e181
philip <philipwafula2@gmail.com>
2020-03-28 17:50:15 +0300

    Adds means to add organization configurations.

--- app/server/utils/organization.py
+++ app/server/utils/organization.py
@@ -1,20 +1,82 @@
+from typing import Dict
+from typing import Optional
+
 from app.server import db
+from app.server.models.configuration import Configuration
 from app.server.models.organization import Organization
 from app.server.schemas.organization import organization_schema
 
 
-def create_organization(name=None):
+def add_organization_configuration(access_control_type=None,
+                                   access_roles=None,
+                                   access_tiers=None,
+                                   organization_id=None):
+    # check that configurations are tied to a specific organization
+    if not organization_id:
+        response = {
+            'error':
+                {'message': 'Configurations must be tied to an organization.',
+                 'status': 'Fail'}}
+        return response, 422
+
+    # check that access control type is defined
+    if not access_control_type:
+        response = {
+            'error':
+                {'message': 'Access control type cannot be empty for an organization\'s configurations.',
+                 'status': 'Fail'}}
+        return response, 422
+
+    if not access_roles:
+        access_roles = []
+
+    if not access_tiers:
+        access_tiers = []
+
+    configurations = Configuration(access_control_type=access_control_type,
+                                   access_roles=access_roles,
+                                   access_tiers=access_tiers,
+                                   organization_id=organization_id)
+    db.session.add(configurations)
+
+    response = {'message': 'Successfully created configurations for organization id {}'.format(organization_id),
+                'status': 'Success'}
+
+    return response, 200
+
+
+def create_organization(configurations: Optional[Dict] = None,
+                        name=None):
     """
     This function creates an organization with attributes  provided.
+    :param configurations:
     :param name: The organization's name.
     :return: An organization object.
     """
     organization = Organization(name=name)
     db.session.add(organization)
+
     # flush because data dump requires to id
     db.session.flush()
 
-    return organization
+    if configurations:
+        access_control_type = configurations.get('access_control_type', None)
+        access_roles = configurations.get('access_roles', None)
+        access_tiers = configurations.get('access_tiers', None)
+
+        response, status_code = add_organization_configuration(access_control_type=access_control_type,
+                                                               access_roles=access_roles,
+                                                               access_tiers=access_tiers,
+                                                               organization_id=organization.id)
+
+        if status_code != 200:
+            return response, status_code
+
+    response = {'data': organization_schema.dump(organization).data,
+                'message': 'Successfully created organization.',
+                'status': 'Success'}
+
+    return response, 200
 
 
 def update_organization(organization, name=None):
@@ -32,11 +94,14 @@ def update_organization(organization, name=None):
 
 def process_create_or_update_organization_request(organization_attributes,
                                                   update_organization_allowed=False):
-    name = organization_attributes.get('name')
+    name = organization_attributes.get('name', None)
+    configurations = organization_attributes.get('configurations', None)
 
     if not name:
-        response = {'error': {'message': 'Organization name cannot be empty.',
-                              'status': 'Fail'}}
+        response = {
+            'error':
+                {'message': 'Organization name cannot be empty.',
+                 'status': 'Fail'}}
         return response, 422
 
     # check if organization exists
@@ -53,13 +118,17 @@ def process_create_or_update_organization_request(organization_attributes,
                         'status': 'Success'}
             return response, 200
         except Exception as exception:
-            response = {'error': {'message': '{}'.format(exception),
-                                  'status': 'Fail'}}
+            response = {
+                'error':
+                    {'message': '{}'.format(exception),
+                     'status': 'Fail'}}
             return response, 400
 
-    organization = create_organization(name=name)
+    response, status_code = create_organization(name=name,
+                                                configurations=configurations)
 
-    response = {'data': organization_schema.dump(organization).data,
-                'message': 'Successfully updated organization.',
-                'status': 'Success'}
-    return response, 200
+    if status_code == 200:
+        db.session.commit()
+        status_code = 201
+
+    return response, status_code
