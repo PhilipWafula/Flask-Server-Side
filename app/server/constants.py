91409ea277da3fc7ab25366b502e3ce96dbfcc20
Philip Wafula <philipwafula2@gmail.com>
2020-03-29 14:17:05 +0300

    Cleans out constants absorbed with organization configs.

--- app/server/constants.py
+++ app/server/constants.py
@@ -1,12 +1,1 @@
 IDENTIFICATION_TYPES = ['NATIONAL_ID', 'PASSPORT']
-
-# standard access roles
-STANDARD_ACCESS_ROLES = ['ADMIN', 'STANDARD_USER']
-
-# tiered access roles
-ADMIN_TIERS = ['SystemAdmin', 'SubAdmin']
-
-# tiered access roles
-TIERED_ACCESS_ROLES = {
-    'ADMIN': ADMIN_TIERS
-}
