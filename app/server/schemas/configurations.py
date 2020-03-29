d71c3465077deba68fc3e91e23054a5c8acd1a13
Philip Wafula <philipwafula2@gmail.com>
2020-03-29 14:16:05 +0300

    Updates respective schema with new attributes.

--- app/server/schemas/configurations.py
+++ app/server/schemas/configurations.py
@@ -5,6 +5,8 @@ from app.server.utils.schemas import BaseSchema
 
 class ConfigurationSchema(BaseSchema):
     access_control_type = fields.Function(lambda configuration: configuration.access_control_type.value)
+    access_roles = fields.List(fields.String())
+    access_tiers = fields.List(fields.String())
 
 
 configuration_schema = ConfigurationSchema()
