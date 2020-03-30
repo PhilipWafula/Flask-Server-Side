a1de3b00d6968f4f6d3c39567afb6a908e831629
Philip Wafula <philipwafula2@gmail.com>
2020-03-30 16:31:25 +0300

    Adds domain attribute to configuration schema.

--- app/server/schemas/configuration.py
+++ app/server/schemas/configuration.py
@@ -7,6 +7,7 @@ class ConfigurationSchema(BaseSchema):
     access_control_type = fields.Function(lambda configuration: configuration.access_control_type.value)
     access_roles = fields.List(fields.String())
     access_tiers = fields.List(fields.String())
+    domain = fields.String()
 
 
 configuration_schema = ConfigurationSchema()
