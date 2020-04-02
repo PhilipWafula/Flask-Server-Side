beb47521db0c68df98343469b738cfbe6a60be50
Philip Wafula <philipwafula2@gmail.com>
2020-04-02 11:33:28 +0300

    Adds supported mailer settings.

--- app/server/constants.py
+++ app/server/constants.py
@@ -1,2 +1,11 @@
 IDENTIFICATION_TYPES = ['NATIONAL_ID', 'PASSPORT']
 SUPPORTED_ACCESS_CONTROL_TYPES = ['STANDARD', 'TIERED']
+SUPPORTED_MAILER_SETTINGS = ['SERVER',
+                             'PORT',
+                             'USE_TSL',
+                             'USE_SSL',
+                             'USERNAME',
+                             'PASSWORD',
+                             'DEFAULT_SENDER',
+                             'MAX_EMAILS',
+                             'USERNAME']
