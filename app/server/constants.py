b2c67d40099161c3bb61a3b77062c95c6e96e757
Philip Wafula <philipwafula2@gmail.com>
2020-04-12 13:09:33 +0300

    Refactors supported mailer settings.

--- app/server/constants.py
+++ app/server/constants.py
@@ -1,11 +1,10 @@
 IDENTIFICATION_TYPES = ['NATIONAL_ID', 'PASSPORT']
 SUPPORTED_ACCESS_CONTROL_TYPES = ['STANDARD', 'TIERED']
-SUPPORTED_MAILER_SETTINGS = ['SERVER',
-                             'PORT',
-                             'USE_TSL',
-                             'USE_SSL',
-                             'USERNAME',
-                             'PASSWORD',
-                             'DEFAULT_SENDER',
+SUPPORTED_MAILER_SETTINGS = ['DEFAULT_SENDER',
                              'MAX_EMAILS',
-                             'USERNAME']
+                             'PASSWORD',
+                             'PORT',
+                             'SERVER',
+                             'USERNAME',
+                             'USE_SSL',
+                             'USE_TSL']
