9ab407801eef4a4b6ace9502a777e38802418f30
SpencerOfwiti <maxspencer56@gmail.com>
2020-06-18 10:41:41 +0300

    Add class for making Africa's Talking payments.

---
+++ app/server/utils/payments/africas_talking.py
@@ -0,0 +1,239 @@
+# system imports
+import re
+from typing import Dict, Optional
+
+# third party import
+from flask import jsonify, make_response
+
+# application imports
+from app import config
+from app.server import constants
+from app.server.templates import responses
+from worker import tasks
+
+
+class AfricasTalking:
+    """This class creates an Africa's Talking payment object with product username and api key."""
+
+    def __init__(self, api_key: str, username: str):
+        self.api_key = api_key
+        self.username = username
+
+    def initiate_mobile_checkout(
+        self,
+        amount: float,
+        phone_number: str,
+        product_name: str,
+        currency_code: str = "KES",
+        metadata: Optional[Dict] = None,
+        provider_channel: Optional[str] = None,
+    ):
+        """This function creates a checkout transaction JSON object with the attributes provided.
+
+        :param amount: The amount to be transacted
+        :param phone_number: The target phone number to complete the transaction
+        :param product_name: The product name initiating the transaction
+        :param currency_code: The currency to be used
+        :param metadata: Additional data
+        :param provider_channel: The channel through which the payment has been initiated from
+        :return: The transaction object
+        """
+        # check that amount is of type float
+        if not isinstance(amount, float):
+            try:
+                amount = float(amount)
+            except ValueError:
+                response, status_code = responses.invalid_amount_type(amount)
+                return make_response(jsonify(response, status_code))
+
+        # check that phone number is in correct MSISDN format
+        if not re.match(config.PHONE_NUMBER_FORMAT, phone_number):
+            response, status_code = responses.invalid_phone_number_format(phone_number)
+            return make_response(jsonify(response), status_code)
+
+        # check that currency is in supported currency codes
+        if currency_code not in constants.AFRICAS_TALKING_CURRENCY_CODES:
+            response, status_code = responses.unsupported_currency_code(currency_code)
+            return make_response(jsonify(response), status_code)
+
+        checkout_transaction = {
+            "amount": amount,
+            "phoneNumber": phone_number,
+            "productName": product_name,
+            "username": self.username,
+            "currencyCode": currency_code,
+        }
+
+        if metadata:
+            checkout_transaction["metadata"] = metadata
+
+        if provider_channel:
+            checkout_transaction["providerChannel"] = provider_channel
+
+        return checkout_transaction
+
+    def initiate_b2b(
+        self,
+        amount: float,
+        destination_account: str,
+        destination_channel: str,
+        product_name: str,
+        provider: str,
+        transfer_type: str,
+        currency_code: str = "KES",
+        metadata: Optional[Dict] = None,
+    ):
+        """This function creates a B2B transaction JSON object with the parameters provided.
+
+        :param amount: The amount to be transacted
+        :param destination_account: The account name that will receive the payment
+        :param destination_channel: The channel that will receive the payment
+        :param product_name: The product name initiating the transaction
+        :param provider: The provider used to process the request
+        :param transfer_type: The transfer type of the payment
+        :param currency_code: The currency to be used
+        :param metadata: Additional data
+        :return: The transaction object
+        """
+        # check that amount is of type float
+        if not isinstance(amount, float):
+            try:
+                amount = float(amount)
+            except ValueError:
+                response, status_code = responses.invalid_amount_type(amount)
+                return make_response(jsonify(response, status_code))
+
+        # check that provider channel is in supported providers
+        if provider not in constants.AFRICAS_TALKING_PROVIDERS:
+            response, status_code = responses.unsupported_provider(provider)
+            return make_response(jsonify(response), status_code)
+
+        # check that transfer type is in supported transfer types
+        if transfer_type not in constants.AFRICAS_TALKING_TRANSFER_TYPES:
+            response, status_code = responses.unsupported_transfer_type(transfer_type)
+            return make_response(jsonify(response), status_code)
+
+        # check that currency is in supported currency codes
+        if currency_code not in constants.AFRICAS_TALKING_CURRENCY_CODES:
+            response, status_code = responses.unsupported_currency_code(currency_code)
+            return make_response(jsonify(response), status_code)
+
+        b2b_transaction = {
+            "amount": amount,
+            "destinationAccount": destination_account,
+            "destinationChannel": destination_channel,
+            "productName": product_name,
+            "provider": provider,
+            "transferType": transfer_type,
+            "username": self.username,
+            "currencyCode": currency_code,
+        }
+
+        if metadata:
+            b2b_transaction["metadata"] = metadata
+
+        return b2b_transaction
+
+    def initiate_b2c(
+        self,
+        amount: float,
+        phone_number: str,
+        product_name: str,
+        currency_code: str = "KES",
+        metadata: Optional[Dict] = None,
+        name: Optional[str] = None,
+        provider_channel: Optional[str] = None,
+        reason: Optional[str] = None,
+    ):
+        """This function creates a B2C transaction JSON object from the parameters provided.
+
+        :param amount: The amount to be transacted
+        :param phone_number: The phone number of the recipient
+        :param product_name: The product name initiating the transaction
+        :param currency_code: The currency to be used
+        :param metadata: Additional data
+        :param name: The name of the recipient
+        :param provider_channel: The channel through which the payment has been initiated from
+        :param reason: The purpose of the payment
+        :return: The transaction object
+        """
+        # check that amount is of type float
+        if not isinstance(amount, float):
+            try:
+                amount = float(amount)
+            except ValueError:
+                response, status_code = responses.invalid_amount_type(amount)
+                return make_response(jsonify(response, status_code))
+
+        # check that phone number is in correct MSISDN format
+        if not re.match(config.PHONE_NUMBER_FORMAT, phone_number):
+            response, status_code = responses.invalid_phone_number_format(phone_number)
+            return make_response(jsonify(response), status_code)
+
+        # check that currency is in supported currency codes
+        if currency_code not in constants.AFRICAS_TALKING_CURRENCY_CODES:
+            response, status_code = responses.unsupported_currency_code(currency_code)
+            return make_response(jsonify(response), status_code)
+
+        # check that reason is in supported reasons
+        if reason and reason not in constants.AFRICAS_TALKING_REASONS:
+            response, status_code = responses.unsupported_reason(reason)
+            return make_response(jsonify(response), status_code)
+
+        recipient = {
+            "amount": amount,
+            "phoneNumber": phone_number,
+            "currencyCode": currency_code,
+        }
+
+        if metadata:
+            recipient["metadata"] = metadata
+
+        if name:
+            recipient["name"] = name
+
+        if provider_channel:
+            recipient["providerChannel"] = provider_channel
+
+        if reason:
+            recipient["reason"] = reason
+
+        recipients = [recipient]
+
+        b2c_transaction = {
+            "productName": product_name,
+            "username": self.username,
+            "recipients": recipients,
+        }
+
+        return b2c_transaction
+
+    def checkout(self, checkout_transaction: Optional[Dict] = None):
+        """This function creates a task for sending a checkout post request.
+
+        :param checkout_transaction: checkout transaction object
+        :return: null
+        """
+        tasks.initiate_africas_talking_checkout.delay(
+            self.api_key, checkout_transaction
+        )
+
+    def b2b(self, b2b_transaction: Optional[Dict] = None):
+        """This function creates a task for sending a B2B post request.
+
+        :param b2b_transaction: B2B transaction object
+        :return: null
+        """
+        tasks.initiate_africas_talking_b2b.delay(self.api_key, b2b_transaction)
+
+    def b2c(self, b2c_transaction: Optional[Dict] = None):
+        """This function creates a task for sending a B2C post request.
+
+        :param b2c_transaction: B2C transaction object
+        :return: null
+        """
+        tasks.initiate_africas_talking_b2c.delay(self.api_key, b2c_transaction)
+
+    def wallet_balance(self):
+        """This function creates a task for sending a wallet balance query."""
+        tasks.initiate_africas_talking_wallet_balance.delay(self.api_key, self.username)
