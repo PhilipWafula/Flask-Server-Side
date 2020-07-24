4396c56abe6cbb8dbdd35bb74bca3276c59d39b6
Philip Wafula <philipwafula2@gmail.com>
2020-07-24 11:12:54 +0300

    Adds get organization by id test

--- tests/unit/app/models/test_organization.py
+++ tests/unit/app/models/test_organization.py
@@ -6,3 +6,9 @@ def test_organization_public_identifier(
 ):
     assert create_master_organization.public_identifier is not None
     assert create_standard_organization.public_identifier is not None
+
+
+def test_get_organization_by_id(test_client, initialize_database, create_master_organization):
+    from app.server.api.organization import get_organization_by_id
+    organization = get_organization_by_id(organization_id=create_master_organization.id)
+    assert organization.name == "Test Master Organization"
