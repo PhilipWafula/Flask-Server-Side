0995374fa8a6956c423650173099c0b0bb24824d
Philip Wafula <philipwafula2@gmail.com>
2020-07-29 14:36:57 +0300

    Add test get master organization.

--- tests/unit/app/models/test_organization.py
+++ tests/unit/app/models/test_organization.py
@@ -12,3 +12,13 @@ def test_get_organization_by_id(test_client, initialize_database, create_master_
     from app.server.api.organization import get_organization_by_id
     organization = get_organization_by_id(organization_id=create_master_organization.id)
     assert organization.name == "Test Master Organization"
+
+
+def test_get_master_organization(test_client, initialize_database, create_master_organization, create_standard_organization):
+    from app.server.models.organization import Organization
+    # check that there are multiple organizations in the database
+    organizations = Organization.query.all()
+    assert len(organizations) > 0
+
+    master_organization = Organization.master_organisation()
+    assert master_organization.name == 'Test Master Organization'
